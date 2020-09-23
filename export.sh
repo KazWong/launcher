7ed28777131ec2a83004ae5c50f52be9375300d1
Kaz@Xavier <whwong@lscm.hk>
2020-09-23 17:14:56 +0800

    become launcher

--- export.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ip=(`ifconfig | grep 'wlan0\|wlo1' -A2`)
-if [ -n "$1" ]; then
-	export ROS_MASTER_URI=http://"$1":11311/
-else
-	export ROS_MASTER_URI=http://${ip[5]}:11311/
-fi
-export ROS_IP=${ip[5]}
